0241cdb61fb999a16257b781193aa3c672cf934b
dibakar102 <dibakar102@yahoo.com>
2016-11-28 00:14:33 -0800

    Update Store.java

--- src/qShop/Store.java
+++ src/qShop/Store.java
@@ -53,7 +53,7 @@ public class Store extends HttpServlet {
         {
         	String url = "jdbc:mysql://cs3.calstatela.edu/cs3220stu28";
             String username = "cs3220stu28";
-            String password = "j.V*CiT.";
+            String password = " ";
             
             c = DriverManager.getConnection( url, username, password );
             Statement stmt = c.createStatement();
@@ -110,7 +110,7 @@ public class Store extends HttpServlet {
         {
         	String url = "jdbc:mysql://cs3.calstatela.edu/cs3220stu28";
             String username = "cs3220stu28";
-            String password = "j.V*CiT.";
+            String password = " ";
             
             c = DriverManager.getConnection( url, username, password );
             
